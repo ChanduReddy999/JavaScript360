629703b8a54718256bd36073eb055bf2c97384c9
V CHANDRA SHEKAR REDDY <113517229+ChanduReddy999@users.noreply.github.com>
2023-06-23 08:15:00 +0530

    Create index.js

---
+++ JavaScript/index.js
@@ -0,0 +1,36 @@
+// javaScript
+
+
+// identifiers
+// datatypes
+// variables
+// constants
+// operators
+// conditional statements
+// unconditional statements
+// looping statements
+
+
+// =====JAVASCRIPT=====
+// arrays
+// strings
+// functions
+// objects
+// json
+// BOM
+// DOM
+// Scope
+// Event-Handling
+// Exception Handling
+// closures
+// hoisting
+// OOPS concepts
+// Form Handling
+// set
+// Map
+// WeakSet
+// WeakMap
+// math module
+// Date
+// Time
+// Regular Expressions
