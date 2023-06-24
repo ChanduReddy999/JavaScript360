68a058b5bb4868ff04ff74cd10d186d7e4abb632
V CHANDRA SHEKAR REDDY <113517229+ChanduReddy999@users.noreply.github.com>
2023-06-24 12:18:27 +0530

    Add files via upload

---
+++ JavaScript/day_01_2_external_js.js
@@ -0,0 +1,6 @@
+// displaying message on the webpage using document.write()
+document.write("hello Chandu");
+// displaying message on the webpage using console.log()
+console.log("Hello Vishnu");
+// displaying message on the webpage using alert()
+alert("hello Mini-World Janu")
---
+++ JavaScript/day_01_3_datatypes.js
@@ -0,0 +1,10 @@
+// working with datatypes in JS
+
+var a=10;       //interger datatype
+document.write(a+"<br>");
+var b=10.23;    //float datatype
+document.write(b+"<br>");
+var ch='chandu';    //string datatype
+document.write(ch+"<br>");
+var b1=true;        //boolean type
+document.write(b1+"<br>");
